d811a090ff18d7a838ce2da1ef2f1fc2f3038d3b
Mark Mikofski <mark.mikofski@dnvgl.com>
2022-12-18 02:32:51 -0800

    clean R, add comments

--- day1part1.R
+++ day1part1.R
@@ -1,17 +1,18 @@
-elf_cals <- 0
-elves_cals <- list()
 f <- file("input-1.txt")
 l <- readLines(f)
 close(f)
-x <- 0
+
+elf_cals <- 0  # current elf's calorie totoal
+elves_cals <- list()  # list of elves calorie totals
+
 for (idx in l) {
     if (idx != "") {
         elf_cals <- elf_cals + strtoi(idx)
     } else {
         elves_cals <- append(elves_cals, elf_cals)
-        #print(elf_cals)
         elf_cals <- 0
     }
 }
+
 max_cals <- max(unlist(elves_cals))
 print(max_cals)
